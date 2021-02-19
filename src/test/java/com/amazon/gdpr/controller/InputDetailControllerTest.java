9d77d1de65ca0258e5909df8ab4f5b3b224ea8ec
Sadhana Sundararajan <susadhan@amazon.com>
2021-02-19 13:10:54 +0530

    Controller code coverage complete

--- src/test/java/com/amazon/gdpr/controller/InputDetailControllerTest.java
+++ src/test/java/com/amazon/gdpr/controller/InputDetailControllerTest.java
@@ -1,15 +1,16 @@
 package com.amazon.gdpr.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.io.IOException;
+
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class InputDetailControllerTest {
@@ -17,21 +18,23 @@ public class InputDetailControllerTest {
 	@InjectMocks
 	private InputDetailController inputDetailController;
 
-	private MockMvc mockMvc;
+	private MockMvc mockMvc;	
+	private MockMvc mock;
 
 	@Before
-	public void setup() {
+	public void setup() throws IOException {
 		MockitoAnnotations.initMocks(this);
-
 		mockMvc = MockMvcBuilders.standaloneSetup(inputDetailController).build();
+		
+		this.mock = MockMvcBuilders.standaloneSetup(inputDetailController)
+                .setViewResolvers(FileUploadControllerTest.viewResolver()).build();	
 	}
 
-	@Test
-	@Ignore
+	@Test	
 	public void inputDetailsTest() throws Exception {
-
-		mockMvc.perform(get("/inputDetailsT")).andExpect(status().isOk()).andExpect(view().name("inputDetails"));
-
+		mock.perform(MockMvcRequestBuilders.get("/inputDetails")).andExpect(status().isOk())
+		.andExpect(view().name("inputDetails"));
+		
 	}
 
 }
--- src/test/java/com/amazon/gdpr/controller/SalesforceDetailControllerTest.java
+++ src/test/java/com/amazon/gdpr/controller/SalesforceDetailControllerTest.java
@@ -1,38 +1,43 @@
 package com.amazon.gdpr.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.io.IOException;
+
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SalesforceDetailControllerTest {
 
 	@InjectMocks
 	private SalesforceDetailController salesforceDetailController;
 
-	private MockMvc mockMvc;
+	private MockMvc mockMvc;	
+	private MockMvc mock;
 
 	@Before
-	public void setup() {
+	public void setup() throws IOException {
 		MockitoAnnotations.initMocks(this);
-
 		mockMvc = MockMvcBuilders.standaloneSetup(salesforceDetailController).build();
+		
+		this.mock = MockMvcBuilders.standaloneSetup(salesforceDetailController)
+                .setViewResolvers(FileUploadControllerTest.viewResolver()).build();	
 	}
 
-	@Test
-	@Ignore
+	@Test	
 	public void salesforceDetailsTest() throws Exception {
-
-		mockMvc.perform(get("/salesforceDetailsT")).andExpect(status().isOk())
-				.andExpect(view().name("salesforceDetails"));
-
+		mock.perform(MockMvcRequestBuilders.get("/salesforceDetails")).andExpect(status().isOk())
+		.andExpect(view().name("salesforceDetails"));
+		
 	}
 
 }
